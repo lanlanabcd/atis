d1a1ccbf14f17dbd14ae71d670525e9a467a8caf
GAREN <garen.pku@gmail.com>
2018-12-27 23:10:15 +0800

    12.27更新 写了个简单版本的encoder和decoder，还没验证正确性，明天继续写

---
+++ myrun.py
@@ -0,0 +1,23 @@
+from parse_args import interpret_args
+import atis_data
+import mymodel
+
+
+def train(params, model, data):
+    input_sequence = [20, 30, 10, 5, 1, 3, 17]
+    expression = model.encode(params, input_sequence)
+    print(expression.npvalue().shape)
+
+
+def main():
+    params = interpret_args()
+    data = atis_data.ATISDataset(params)
+    # model_level代表模型复杂程度，目前在实现level0，即普通ENCODER-DECODER模型
+    if params.model_level ==0:
+        model = mymodel.UtteranceModel(params, data.input_vocabulary, data.output_vocabulary)
+    if params.train:
+        train(params, model, data)
+
+
+if __name__ == "__main__":
+    main()
